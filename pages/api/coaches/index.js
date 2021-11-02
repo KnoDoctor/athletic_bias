6c5657bfe6d45ae429b3c7db50c73d72aa681b63
Jeff Barfield <jeff.barfield@butterfield.com>
2021-11-02 00:36:01 -0400

    Signed-off-by: Jeff Barfield <jeff.barfield@butterfield.com>

--- pages/api/coaches/index.js
+++ pages/api/coaches/index.js
@@ -10,6 +10,8 @@ import { generateGuid } from "../../../utils/uuids";
 const cors = initMiddleware(
     // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
     Cors({
+        // Allow Origin
+        origin: "*",
         // Only allow requests with GET, POST and OPTIONS
         methods: ["GET", "POST", "OPTIONS"],
     })
